a4b06f717bdf94f39ae8c48ce417024d2a24f492
unknown <>
2017-06-07 13:27:16 +0800

    the sum of numbers in an array“

--- spec/jasmine_examples/PlayerSpec.js
+++ spec/jasmine_examples/PlayerSpec.js
@@ -1,18 +1,18 @@
 
- describe('Player()', function() {
-     it('the mod of two numbers',function() {
+ describe("Player", function() {
+     var Player = require('../../lib/jasmine_examples/Player');
+
+     it("the mod of two numbers",function() {
          expect(Player.calMod(9, 3)).toEqual(0);
          expect(Player.calMod(9,4)).toEqual(1);
      });
 
-     /*it('should calculate the sum of numbers which are in array', () => {
-         let numArray1 = [3, 6, 1, 8, 6, 4, 2];
-         let numArray2 = [1, 2, 6, 2, 9, 0, 4];
-         expect(Player.calSum(numArray1)).toBe(30);
-         expect(Player.calSum(numArray2)).toBe(24);
+     it('calculate the sum of numbers in an array', () => {
+         let numArray1 = [1,2,3];
+         expect(Player.calSum(numArray1)).toBe(6);
      });
 
-     it('should calculate the sum of numbers which are in array and less than indicated num ', () => {
+     /*it('should calculate the sum of numbers which are in array and less than indicated num ', () => {
          let numArray1 = [3, 6, 1, 8, 6, 4, 2];
          let numArray2 = [1, 2, 6, 2, 9, 0, 4];
 
